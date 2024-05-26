e34c631b324257de430a5c51d608be59bcae2f86
Gustavo Silva <tavodiassilva@gmail.com>
2024-05-26 18:02:07 -0300

    feat: create Entity Doctor & Address

---
+++ src/main/java/med/voll/api/doctors/Doctor.java
@@ -0,0 +1,40 @@
+package med.voll.api.doctors;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.address.Address;
+
+@Table(name = "doctors")
+@Entity(name= "Doctor")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Doctor {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Address address;
+
+
+    
+
+
+}
