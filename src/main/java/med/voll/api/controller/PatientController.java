da438e17d89146b05267a746e19d475346de7f6c
Gustavo Silva <tavodiassilva@gmail.com>
2024-06-17 18:27:38 -0300

    feat: adding Getting Patient

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.patients.ListPatients;
 import med.voll.api.patients.Patient;
 import med.voll.api.patients.PatientRegisterData;
 import med.voll.api.patients.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -24,6 +24,11 @@ public class PatientController {
     @Transactional
     public void createPatient(@RequestBody @Valid PatientRegisterData data){
         repository.save(new Patient(data));
+    }
+
+    @GetMapping
+    public List<ListPatients> ListPatients(){
+        return repository.findAll().stream().map(ListPatients::new).toList();
     }
 
 }
